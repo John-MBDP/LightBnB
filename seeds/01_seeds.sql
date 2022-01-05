c998d294847c737926eccbec80518051883bed8b
John <johnartuz@hotmail.com>
2022-01-05 18:38:59 -0500

    Add fake data for users table

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password)
+VALUES ('John', 'John@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Joshua', 'Joshua@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Mark', 'Mark@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Neil', 'Neil@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
