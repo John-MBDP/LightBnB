5c3292e4714df464d77040987fd9e7c8860b64e0
John <johnartuz@hotmail.com>
2022-01-05 18:53:24 -0500

    Create fake data for properties

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,5 +1,10 @@
 INSERT INTO users (name, email, password)
 VALUES ('John', 'John@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('Joshua', 'Joshua@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Mark', 'Mark@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Neil', 'Neil@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+('Mark', 'Mark@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, country, street, city, province, post_code)
+VALUES ('1', 'title1', 'description', '//images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 150, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680'),
+('2', 'title2', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 200, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583'),
+('3', 'title3', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 90, 'Canada', '513 Powov Grove ', 'Montreal', 'Quebec', '38051');
