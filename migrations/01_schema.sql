fa498882bfc56bd98c83b76cdc4032b7d899e8e4
John <johnartuz@hotmail.com>
2022-01-05 17:46:17 -0500

    Create table for reservations

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -26,3 +26,11 @@ CREATE TABLE properties (
 
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
